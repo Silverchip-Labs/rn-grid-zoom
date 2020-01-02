05c4557928fada9d7b48d07f9b7063a165877738
Oliver <oli@silverchip.com>
2020-01-02 17:01:14 +0000

    add red boxes

--- example/App.js
+++ example/App.js
@@ -5,27 +5,38 @@ import ImageZoom from './built/index';
 import chessboard from './assets/chessboard/chessboard.jpg';
 import blackSquare from './assets/chessboard/black.jpg';
 import whiteSquare from './assets/chessboard/white.jpg';
+import bee from './assets/bee.png';
 
 const IMAGE_HEIGHT = 1000;
 const IMAGE_WIDTH = 1000;
-const IMAGE_PADDING = 100;
+const IMAGE_PADDING = 1000;
 const IMAGE_OVERFLOW = IMAGE_PADDING * 2;
 
-const MIN_SCALE = 0.2;
+const MIN_SCALE = 0.04;
 const MAX_SCALE = 5;
-const SCALE_BREAKPOINT = 1.5;
+const SCALE_BREAKPOINT = 0.1;
 
 export default class App extends React.Component {
-  state = {
-    renderBig: true
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      renderBig: true,
+      beeX: 0,
+      beeY: 0
+    };
+
+    this.imgZoomRef = React.createRef();
+    this.dimensionHeight = Dimensions.get('window').height;
+    this.dimensionWidth = Dimensions.get('window').width;
+  }
 
   render() {
     const { renderBig } = this.state;
     return (
       <ImageZoom
-        cropWidth={Dimensions.get('window').width}
-        cropHeight={Dimensions.get('window').height}
+        ref={this.imgZoomRef}
+        cropWidth={this.dimensionWidth}
+        cropHeight={this.dimensionHeight}
         imageWidth={IMAGE_WIDTH * 8 + IMAGE_OVERFLOW}
         imageHeight={IMAGE_WIDTH * 8 + IMAGE_OVERFLOW}
         minScale={MIN_SCALE}
@@ -41,6 +52,16 @@ export default class App extends React.Component {
             paddingVertical: IMAGE_PADDING
           }}
         >
+          <Image
+            source={bee}
+            style={{
+              width: 300,
+              height: 300,
+              left: this.state.beeX,
+              top: this.state.beeY,
+              position: 'absolute'
+            }}
+          />
           {renderBig ? (
             <Image
               enableHorizontalBounce={true}
@@ -60,27 +81,47 @@ export default class App extends React.Component {
 
   handleMove = params => {
     const renderBig = params.scale < SCALE_BREAKPOINT;
-    console.log({ renderBig, ...params });
-    if (this.state.renderBig != renderBig) {
+    if (params.type === 'onPanResponderRelease') {
+      console.log(params);
+    }
+
+    if (this.state.renderBig !== renderBig) {
       this.setState({ renderBig });
     }
+
+    this.setState({
+      beeX: params.positionX,
+      beeY: params.positionY
+    });
   };
 
   _renderRows = () => {
-    return [1, 2, 3, 4, 5, 6, 7, 8].map(n => (
-      <View style={{ flexDirection: 'row' }}>{n % 2 ? this._renderEvenRow() : this._renderOddRow()}</View>
+    return [1, 2, 3, 4, 5, 6, 7, 8].map(row => (
+      <View style={{ flexDirection: 'row' }}>{row % 2 ? this._renderEvenRow(row) : this._renderOddRow(row)}</View>
     ));
   };
 
-  _renderOddRow = () => {
-    return [1, 2, 3, 4, 5, 6, 7, 8].map(n => this._renderBox(n % 2 ? blackSquare : whiteSquare));
+  _renderOddRow = rowNum => {
+    return [1, 2, 3, 4, 5, 6, 7, 8].map(col => this._renderBox(col % 2 ? blackSquare : whiteSquare, rowNum, col));
   };
 
-  _renderEvenRow = () => {
-    return [1, 2, 3, 4, 5, 6, 7, 8].map(n => this._renderBox(n % 2 ? whiteSquare : blackSquare));
+  _renderEvenRow = rowNum => {
+    return [1, 2, 3, 4, 5, 6, 7, 8].map(col => this._renderBox(col % 2 ? whiteSquare : blackSquare, rowNum, col));
   };
 
-  _renderBox = source => {
-    return <Image style={{ height: IMAGE_HEIGHT, width: IMAGE_WIDTH }} source={source} />;
+  // 2, 3
+  _renderBox = (source, rowNum, colNum) => {
+    // 2000, 3000
+    const { current } = this.imgZoomRef;
+    const { positionX, positionY, scale } = current;
+    const { dimensionHeight: height, dimensionWidth: width } = this;
+
+    // todo get scale offset for x and y, height width and scale
+    // todo check if box
+
+    const shouldRender = colNum % 2;
+    if (shouldRender) return <Image style={{ height: IMAGE_HEIGHT, width: IMAGE_WIDTH }} source={source} />;
+
+    return <View style={{ height: IMAGE_HEIGHT, width: IMAGE_WIDTH, backgroundColor: 'red' }} />;
   };
 }
