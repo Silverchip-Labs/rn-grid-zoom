b3f04655f69d82219af9d150cb75d2965f256f23
Rob Fairclough <rob@silverchip.com>
2020-01-03 11:37:38 +0000

    testing with floorplan images

--- example/App.js
+++ example/App.js
@@ -5,30 +5,29 @@ import ImageZoom from './built/index';
 import chessboard from './assets/chessboard/chessboard.jpg';
 import blackSquare from './assets/chessboard/black.jpg';
 import whiteSquare from './assets/chessboard/white.jpg';
+import onek from './assets/1kx1k.png';
+import fullMap from './assets/orig.png';
 import bee from './assets/bee.png';
 
-const IMAGE_HEIGHT = 1000;
-const IMAGE_WIDTH = 1000;
-const IMAGE_PADDING = 1000;
+const IMAGE_HEIGHT = 2250;
+const IMAGE_WIDTH = 2250;
+const IMAGE_PADDING = 100;
 const IMAGE_OVERFLOW = IMAGE_PADDING * 2;
 
-const MIN_SCALE = 0.04;
+const MIN_SCALE = 0.15;
 const MAX_SCALE = 5;
-const SCALE_BREAKPOINT = 0.1;
+const SCALE_BREAKPOINT = 0.5;
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      renderBig: true,
-      beeX: 0,
-      beeY: 0
-    };
+  state = {
+    renderBig: true,
+    beeX: 0,
+    beeY: 0
+  };
 
-    this.imgZoomRef = React.createRef();
-    this.dimensionHeight = Dimensions.get('window').height;
-    this.dimensionWidth = Dimensions.get('window').width;
-  }
+  imgZoomRef = React.createRef();
+  dimensionHeight = Dimensions.get('window').height;
+  dimensionWidth = Dimensions.get('window').width;
 
   render() {
     const { renderBig } = this.state;
@@ -37,8 +36,8 @@ export default class App extends React.Component {
         ref={this.imgZoomRef}
         cropWidth={this.dimensionWidth}
         cropHeight={this.dimensionHeight}
-        imageWidth={IMAGE_WIDTH * 8 + IMAGE_OVERFLOW}
-        imageHeight={IMAGE_WIDTH * 8 + IMAGE_OVERFLOW}
+        imageWidth={IMAGE_WIDTH + IMAGE_OVERFLOW}
+        imageHeight={IMAGE_WIDTH + IMAGE_OVERFLOW}
         minScale={MIN_SCALE}
         maxScale={MAX_SCALE}
         onMove={this.handleMove}
@@ -46,8 +45,8 @@ export default class App extends React.Component {
         <View
           pointerEvents={'none'}
           style={{
-            width: IMAGE_WIDTH * 8 + IMAGE_OVERFLOW,
-            height: IMAGE_HEIGHT * 8 + IMAGE_OVERFLOW,
+            width: IMAGE_WIDTH + IMAGE_OVERFLOW,
+            height: IMAGE_HEIGHT + IMAGE_OVERFLOW,
             paddingHorizontal: IMAGE_PADDING,
             paddingVertical: IMAGE_PADDING
           }}
@@ -66,10 +65,10 @@ export default class App extends React.Component {
             <Image
               enableHorizontalBounce={true}
               style={{
-                width: IMAGE_HEIGHT * 8,
-                height: IMAGE_HEIGHT * 8
+                width: IMAGE_WIDTH,
+                height: IMAGE_HEIGHT
               }}
-              source={chessboard}
+              source={fullMap}
             />
           ) : (
             this._renderRows()
@@ -102,16 +101,14 @@ export default class App extends React.Component {
   };
 
   _renderOddRow = rowNum => {
-    return [1, 2, 3, 4, 5, 6, 7, 8].map(col => this._renderBox(col % 2 ? blackSquare : whiteSquare, rowNum, col));
+    return [1, 2, 3, 4, 5, 6, 7, 8].map(col => this._renderBox(col % 2 ? onek : onek, rowNum, col));
   };
 
   _renderEvenRow = rowNum => {
-    return [1, 2, 3, 4, 5, 6, 7, 8].map(col => this._renderBox(col % 2 ? whiteSquare : blackSquare, rowNum, col));
+    return [1, 2, 3, 4, 5, 6, 7, 8].map(col => this._renderBox(col % 2 ? onek : onek, rowNum, col));
   };
 
-  // 2, 3
   _renderBox = (source, rowNum, colNum) => {
-    // 2000, 3000
     const { current } = this.imgZoomRef;
     const { positionX, positionY, scale } = current;
     const { dimensionHeight: height, dimensionWidth: width } = this;
@@ -119,9 +116,9 @@ export default class App extends React.Component {
     // todo get scale offset for x and y, height width and scale
     // todo check if box
 
-    const shouldRender = colNum % 2;
-    if (shouldRender) return <Image style={{ height: IMAGE_HEIGHT, width: IMAGE_WIDTH }} source={source} />;
+    const shouldRender = true;
+    if (shouldRender) return <Image style={{ height: IMAGE_HEIGHT / 9, width: IMAGE_WIDTH / 9 }} source={source} />;
 
-    return <View style={{ height: IMAGE_HEIGHT, width: IMAGE_WIDTH, backgroundColor: 'red' }} />;
+    return <View style={{ height: IMAGE_HEIGHT / 9, width: IMAGE_WIDTH / 9, backgroundColor: 'red' }} />;
   };
 }
